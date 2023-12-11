45e13775552637128813148f34b4b16b1de745dc
Kumari Saumya <kumarisaumya@192.168.1.17>
2023-12-11 14:10:57 +0530

    pattern example

--- js/pattern/pattern.js
+++ js/pattern/pattern.js
@@ -15,7 +15,6 @@ function pattern0(n){
  
 }
 console.log(pattern0(5))
-
 /* Input Format: N = 3
 Result: 
 * 
@@ -122,3 +121,111 @@ function pattern5(n){
  
 }
 console.log(pattern5(3))
+
+/* Input Format: N = 3
+Result: 
+  *  
+ *** 
+*****   
+*/
+// 1  = 2,1,2
+// 2 = 1,3,1
+//3 = 0,5,0
+// space = n-i
+// star = 2i-1
+// space, star , space
+
+function pattern6(n){
+    for(let i=1;i<=n;i++){
+        for(let j=1;j<=n-i;j++){
+            process.stdout.write(' ');
+          }
+          for(let j=1;j<=2*i-1;j++){
+            process.stdout.write('*');
+          }
+          for(let j=1;j<=n-i;j++){
+            process.stdout.write(' ');
+          }
+        console.log('\n');
+    }
+
+ 
+}
+console.log(pattern6(3))
+
+/* Input Format: N = 3
+Result: 
+*****  
+ ***
+  *    
+  * 
+1 = 0,5,0
+2 = 1,3,1
+3= 2,1,2
+
+space = i-1
+star = n-2i+4
+
+n = 2i-4
+n = 2(i-2)
+2n = i-2
+2n-i+2
+ 
+ */
+
+function pattern7(n){
+    for(let i=1;i<=n;i++){
+        for(let j=1;j<=i-1;j++){
+            process.stdout.write(' ');
+          }
+          for(let j=1;j<=n-(2*i)+4;j++){
+            process.stdout.write('*');
+          }
+          for(let j=1;j<=i-1;j++){
+            process.stdout.write(' ');
+          }
+        console.log('\n');
+    }
+
+ 
+}
+console.log(pattern7(3))
+
+/* Input Format: N = 3
+Result: 
+  *  
+ ***
+***** 
+*****  
+ ***
+  *    */
+
+ function x(){
+    pattern6();
+    pattern7();
+ }
+x()
+
+
+/* Input Format: N = 3
+Result: 
+  *  
+  **
+  ***  
+  **
+  *   */
+
+  function pattern8(n){
+    for(let i=1;i<=2*n-1;i++){
+        let star = i;
+        if(i > n) star = 2*n-i
+        for(let j=1;j<=star;j++){
+            process.stdout.write('*');
+          }
+          
+        console.log('\n');
+    }
+
+ 
+}
+console.log(pattern8(3))

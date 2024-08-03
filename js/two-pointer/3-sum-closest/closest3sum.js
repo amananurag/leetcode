f8ef31cadb5da5bd14361c062fff1c3d4dcef65a
Kumari Saumya <kumarisaumya@192.168.1.11>
2024-08-03 16:03:24 +0530

    3 sum closest

---
+++ js/two-pointer/3-sum-closest/closest3sum.js
@@ -0,0 +1,40 @@
+
+// https://leetcode.com/problems/3sum-closest/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a,b)=> a-b);
+    let mindiff = Infinity;
+    for(let i=0;i<nums.length-2;i++){
+        let low = i+1;
+        let high = nums.length-1;
+        while(low < high){
+            let sum = nums[i]+nums[low]+nums[high];
+            let diff = Math.abs(target - sum);
+           // mindiff = Math.min(mindiff, diff);
+            if (diff < Math.abs(target - mindiff)) {
+                mindiff = sum;
+            }
+            if(sum === target){
+            while(nums[low]===nums[low+1]) low++;
+            while(nums[high]===nums[high-1]) high--;
+            low++;
+            high--;
+            return sum;
+            }
+           
+            if (sum < target) {
+                low++;
+            } else if (sum > target) {
+                high--;
+            }
+            
+           
+        }
+        while(nums[i]===nums[i+1])i++;
+    }
+    return mindiff;
+};

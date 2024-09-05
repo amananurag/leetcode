928e8b57754c377f7f485668918302bc7b3c2c64
Kumari Saumya <kumarisaumya@Kumaris-MacBook-Air.local>
2024-09-05 16:28:01 +0530

    dfs example added

---
+++ js/tree/dfs/postorder/postorder.js
@@ -0,0 +1,42 @@
+// testInorderTraversal.js
+
+// Import the function (if in a different file)
+// const { inorderTraversal } = require('./yourFileName');
+
+// Define the TreeNode class
+// Binary Tree Inorder Traversal
+// https://leetcode.com/problems/binary-tree-inorder-traversal/description/?envType=problem-list-v2&envId=tree
+function TreeNode(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+}
+
+// Function to be tested
+var inorderTraversal = function(root) {
+    let res = [];
+    inorder(root, res);
+    return res;
+};
+
+function inorder(node, res) {
+    if (!node) return null;
+  
+    inorder(node.left, res);
+ 
+    inorder(node.right, res);
+    res.push(node.val);
+}
+
+// Test cases
+const root1 = new TreeNode(1, null, new TreeNode(2, new TreeNode(3)));
+console.log(inorderTraversal(root1)); // Output: [1, 3, 2]
+
+const root2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+console.log(inorderTraversal(root2)); // Output: [2, 1, 3]
+
+const root3 = null;
+console.log(inorderTraversal(root3)); // Output: []
+
+const root4 = new TreeNode(1);
+console.log(inorderTraversal(root4)); // Output: [1]

59fec94fb128627adcca6a9b99550c814f086496
Kumari Saumya <kumarisaumya@192.168.1.14>
2024-12-11 12:49:27 +0530

    add binary using bigint

---
+++ js/bitmanipulation/addbinary.js
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/add-binary/submissions/1475922253/
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let sum1 = BigInt("0b"+a);
+    let sum2 = BigInt("0b"+b);
+    let sum = sum1+sum2;
+    return sum.toString(2);
+};
+
+
+console.log(addBinary("11","1"))
+console.log(addBinary("1010","1011"))

725d585ff740d219265dba3ede59f91d964a87fc
Kumari Saumya <kumarisaumya@192.168.1.12>
2025-03-31 08:47:00 +0530

    refactored

--- js/array/sort/topkfreqelement/topkfreqelement.js
+++ js/array/sort/topkfreqelement/topkfreqelement.js
@@ -9,11 +9,7 @@ var topKFrequent = function(nums, k) {
   
     let obj ={};
     for(let i=0;i<nums.length;i++){
-        if(obj[nums[i]]){
-            obj[nums[i]]++;
-        }else{
-            obj[nums[i]] =1;
-        }
+      obj[nums[i]] = ( obj[nums[i]] || 0)   + 1;
     }
 
     let res = Object.entries(obj);

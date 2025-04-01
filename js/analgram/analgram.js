35fc2f3ca6106341b49d0bc3563b28df2d25974b
Kumari Saumya <kumarisaumya@192.168.1.12>
2025-04-01 10:03:27 +0530

    refactored

--- js/analgram/analgram.js
+++ js/analgram/analgram.js
@@ -8,18 +8,11 @@ function analgram(word1, word2){
     const obj2= {};
     for(let char of lowerWord1){
 
-        if(obj1[char]){
-            obj1[char]++;
-        }else{
-            obj1[char]=1;
-        }
+        obj1[char] = (obj1[char] || 0 ) + 1;
     }
     for(let char of lowerWord2){
-        if(obj2[char]){
-            obj2[char]++;
-        }else{
-            obj2[char]=1;
-        }
+        obj2[char] = (obj2[char] || 0 ) + 1;
+       
     }
     for(let key in obj1){
         if(!obj2[key] || obj1[key] !== obj2[key]){

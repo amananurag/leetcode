a00e58439b657a17a368c433f15942af7869493e
Kumari Saumya <kumarisaumya@192.168.1.40>
2025-04-25 12:05:22 +0530

    reverse vowel added

--- js/string/reverse-vowel.js
+++ js/string/reverse-vowel.js
@@ -2,12 +2,38 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
+var reverseVowels1 = function(s) {
     const vow = s.match(/[aeiou]/gi);
    return s.replace(/[aeiou]/ig, el=> vow.pop());
 };
 
 
-console.log(reverseVowels("IceCreAm"))
+console.log(reverseVowels1("IceCreAm"))
 
-console.log(reverseVowels("leetcode"))
+console.log(reverseVowels1("leetcode"))
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    const arr = s.split('');
+    let left = 0, right = arr.length - 1;
+
+    while (left < right) {
+        while (left < right && !vowels.has(arr[left])) left++;
+        while (left < right && !vowels.has(arr[right])) right--;
+
+        // Swap vowels
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+
+    return arr.join('');
+};
+
+console.log(reverseVowels("IceCreAm"));   // Output: "AecCrEmI"
+console.log(reverseVowels("leetcode"));   // Output: "leotcede"

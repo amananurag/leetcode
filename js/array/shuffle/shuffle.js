f0fc23d283c46c8f5ef35a3243c480d70b967092
Kumari Saumya <kumarisaumya@192.168.1.9>
2025-05-21 04:41:25 +0530

    shuffle n/2

--- js/array/shuffle/shuffle.js
+++ js/array/shuffle/shuffle.js
@@ -1,19 +1,9 @@
-
-    var shuffle = function(nums, n) {
-        let temp =[];
-        while(nums.length){
-            temp.push(nums.splice(0,n))
-        }
-       let temp1 = temp[0];
-       let temp2 = temp[1];
-         let result = [];
-        for(let i=0;i<n;i++){
-            result.push(temp1[i]);
-            result.push(temp2[i]);  
-
-    };
+var shuffle = function(nums, n) {
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        result.push(nums[i], nums[i + n]);
+    }
     return result;
-}
+};
 
-
-    console.log(shuffle([2,5,1,3,4,7],3))
+console.log(shuffle([2,5,1,3,4,7], 3)); // Output: [2,3,5,4,1,7]

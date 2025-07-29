d65781a2ea35439edc9af2c125532e7d6e7dc340
Kumari Saumya <kumarisaumya@192.168.1.14>
2025-07-29 12:55:19 +0530

    unbounded knapsack

--- js/backtracking/memo-bt/coin-combinations.js
+++ js/backtracking/memo-bt/coin-combinations.js
@@ -30,3 +30,28 @@ var coinChange = function(coins, amount) {
 
 
  console.log(coinChange([1,3,5],12))
+
+
+ var change = function(amount, coins) {
+    const memo = new Map();
+
+    function dp(index, rem) {
+        if (rem === 0) return 1;
+        if (index === coins.length || rem < 0) return 0;
+
+        const key = `${index}-${rem}`;
+        if (memo.has(key)) return memo.get(key);
+
+        // 2 options: pick or not pick
+        let pick = dp(index, rem - coins[index]); // stay on same index
+        let skip = dp(index + 1, rem); // move to next coin
+
+        memo.set(key, pick + skip);
+        return pick + skip;
+    }
+
+    return dp(0, amount);
+};
+
+
+console.log(change([1,3,5],12))
